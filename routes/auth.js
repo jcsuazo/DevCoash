9249fda88f4864760907f9ec78b65dfeeafbf1cf
jcsuazo <jsuazo9@gmail.com>
2021-06-26 18:29:12 -0400

    added route protection and authorze middleware

--- routes/auth.js
+++ routes/auth.js
@@ -13,15 +13,15 @@ const {
 
 const router = express.Router();
 
-// const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 router.get('/confirmemail', confirmEmail);
-router.put('/updatedetails', updateDetails);
-router.put('/updatepassword', updatePassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
