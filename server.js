13a2f38e12f15cb1704a135287cee24aed2d921d
jcsuazo <jsuazo9@gmail.com>
2021-06-27 07:53:31 -0400

    added reviews model, controller and seeder

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -45,6 +46,7 @@ app.use(`${version}/bootcamps`, bootcamps);
 app.use(`${version}/courses`, courses);
 app.use(`${version}/auth`, auth);
 app.use(`${version}/users`, users);
+app.use(`${version}/reviews`, reviews);
 
 app.get('/', (request, response) => {
   response.send('Hello from express');
