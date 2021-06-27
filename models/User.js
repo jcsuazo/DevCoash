f0e4abca868bf43fed2ad2c1deeb00e98436d914
jcsuazo <jsuazo9@gmail.com>
2021-06-26 20:41:07 -0400

    add reset password, change email and password, send forget password email

--- models/User.js
+++ models/User.js
@@ -71,21 +71,21 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // // Generate and hash password token
-// UserSchema.methods.getResetPasswordToken = function () {
-//   // Generate token
-//   const resetToken = crypto.randomBytes(20).toString('hex');
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
 
-//   // Hash token and set to resetPasswordToken field
-//   this.resetPasswordToken = crypto
-//     .createHash('sha256')
-//     .update(resetToken)
-//     .digest('hex');
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
 
-//   // Set expire
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
-//   return resetToken;
-// };
+  return resetToken;
+};
 
 // // Generate email confirm token
 // UserSchema.methods.generateEmailConfirmToken = function (next) {
